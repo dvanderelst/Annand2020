c9e6c249581b7205421dcb0e9984227cc814b8ca
Lenovo <dieter.vanderelst@fastmail.net>
2022-03-08 19:47:02 -0500

    removed latex

--- library/Settings.py
+++ library/Settings.py
@@ -2,4 +2,4 @@ import matplotlib.pyplot as plt
 
 plt.rcParams['text.usetex'] = True
 
-image_folder = '/home/dieter/Dropbox/PythonRepos/Letter2Editor/latex/images'
+image_folder = '/home/dieter/Dropbox/Apps/Overleaf/Letter2Editor/images'
